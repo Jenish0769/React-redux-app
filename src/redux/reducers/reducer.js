004efca51f9679a2149c5ebd945e7f212a94fe8d
jenish Hirpara <jenishhirpara0769@gmail.com>
2022-07-27 16:09:51 +0530

    counter changes

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,14 +1,14 @@
 import { INCREMENT_VALUE, DECREMENT_VALUE, INITIAL_VALUE } from "../constants/constant";
 
 export const counterReducer = (state = 0, action) => {
-    let previousState = state;
+    
     switch (action.type) {
         case INCREMENT_VALUE:
-            return (previousState) => previousState + 1;
+            return state + 1;
         case DECREMENT_VALUE:
-            return (previousState) => previousState + 1;
+            return state - 1;
         case INITIAL_VALUE:
-            return state;
+            return (state=0);
         default:
             return state;
     }
